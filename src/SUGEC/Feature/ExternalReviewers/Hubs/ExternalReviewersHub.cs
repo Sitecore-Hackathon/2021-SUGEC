4ba4d3d301a9bcea12419d9c8506a4ff1ca2e313
Santiago Morla <santiago.morla@verndale.com>
2021-03-06 11:32:37 -0500

    Added SignalR to the messaging.

---
+++ src/SUGEC/Feature/ExternalReviewers/Hubs/ExternalReviewersHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace ExternalReviewers.Hubs
+{
+    public class ExternalReviewersHub : Hub
+    {
+        public async Task CommentsHub(string user, string body, string date)
+        {
+            await Clients.All.SendAsync("ReceiveComment", user, body, date);
+        }
+    }
+}
